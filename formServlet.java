f89e06942066d68bf315810e5bcea1641368291b
holson1 <henryolson1@gmail.com>
2014-01-31 18:54:00 -0800

    Committed by qcommit.sh

--- formServlet.java
+++ formServlet.java
@@ -21,7 +21,7 @@ public class formServlet extends HttpServlet {
 		String name = request.getParameter("name");
 		
 		if(name != null)
-			out.println("How's it going, " + name + "?\n");
+			out.println("How are you, " + name + "?\n");
 		else
 			out.println("You didn't enter any text!\n");
 		
---
+++ holsonLab.java
@@ -0,0 +1,10 @@
+
+public class holsonLab {
+	/**
+	* When run, the main method will print "Hello World!" to the console.
+	* @param args
+	*/
+	public static void main(String[] args) {
+	System.out.println("Hello World!");
+	}
+}
