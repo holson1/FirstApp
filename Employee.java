a9517c4df41166a9eefc6915bf8ffdb6ac08c220
holson1 <henryolson1@gmail.com>
2014-02-27 15:32:41 -0800

    Committed by qcommit.sh

---
+++ Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee {
+
+	public String firstName;
+	public String lastName;
+	public String position;
+	
+	public Employee(String new_fname, String new_lname, String new_pos) {
+		firstName = new_fname;
+		lastName = new_lname;
+		position = new_pos;
+	}
+	
+}
+
--- formServlet.java
+++ formServlet.java
@@ -3,9 +3,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class formServlet extends HttpServlet {
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	public void doPost(HttpServletRequest request,
@@ -16,14 +14,20 @@ public class formServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		out.println("<title>FirstServletResponse</title>");
-		out.println("<h2>Button Clicked</h2>");
 		
-		String name = request.getParameter("name");
+		String firstName = request.getParameter("fname");
+		String lastName = request.getParameter("lname");
+		String title = request.getParameter("title");
 		
-		if(name != null)
-			out.println("How are you, " + name + "?\n");
-		else
-			out.println("You didn't enter any text!\n");
+		//if any of the fields are empty
+		if(firstName.isEmpty() || lastName.isEmpty() || title.isEmpty()) {
+			System.out.println("<p>Information could not be saved. Please enter information for "
+					+ "all fields.</p>");
+			System.exit(0);
+		}
+		
+		out.println("<h2>A new employee has been created.</h2>");
+		out.println("<p>" + firstName + " " + lastName + ", " + title + "</p>");
 		
 		out.println("<a href=\"../../index.html\">"
 				+ "Return to form</a>");
